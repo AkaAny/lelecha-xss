b275a0e46f0e67c0c24073ec819a12e08cc96efd
Any <fkd_any@qq.com>
2024-03-23 13:44:17 +0800

    init project

---
+++ lelecha.js
@@ -0,0 +1,7 @@
+!(function(){
+  // const baseTag= document.getElementById("d");
+  // baseTag.parentElement.removeChild(baseTag);
+  console.log("hello");
+  console.log(document.location.href);
+  throw "ex";
+})()
